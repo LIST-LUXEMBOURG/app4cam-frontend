317d46344e95f6f385315ba2c0d6b42d705dc5ec
Daniel Waxweiler <daniel.waxweiler@list.lu>
2022-02-08 09:09:12 +0100

    show device ID and site name on home page

---
+++ src/views/DashboardView.spec.ts
@@ -0,0 +1,25 @@
+import { mount } from '@vue/test-utils'
+import DashboardView from './DashboardView.vue'
+import { key } from '../store'
+import { nextTick } from 'vue'
+
+it('displays device ID and site name', async () => {
+  const store = {
+    state: {
+      deviceId: 'd',
+      siteName: 's',
+    },
+    dispatch: jest.fn(() => Promise.resolve()),
+  }
+  const wrapper = mount(DashboardView, {
+    global: {
+      provide: {
+        _q_: undefined,
+        [key as symbol]: store,
+      },
+    },
+  })
+  await nextTick()
+  const input = wrapper.find('[data-test-id=deviceInformation]')
+  expect(input.text()).toBe(store.state.siteName + ' ' + store.state.deviceId)
+})
