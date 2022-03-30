f92516c6ca7cae975ea7f91f7aace6ff095f16c2
Daniel Waxweiler <daniel.waxweiler@list.lu>
2022-03-30 10:09:53 +0200

    add commit hash to footer

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+import { execSync } from 'child_process'
+
+const commitHash = execSync('git rev-parse --short HEAD').toString()
 
 export default defineConfig({
   define: {
     __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
+    __COMMIT_HASH__: JSON.stringify(commitHash.trim()),
   },
   plugins: [
     vue({
