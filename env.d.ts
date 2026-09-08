7ccf3a4eefc047cf1f60d1682dd1866e7ffd094d
Daniel Waxweiler <daniel.waxweiler@list.lu>
2026-09-08 16:03:37 +0200

    add missing copyright notice

--- env.d.ts
+++ env.d.ts
@@ -1,4 +1,21 @@
 /**
+ * Copyright (C) since 2026 Luxembourg Institute of Science and Technology
+ *
+ * App4Cam is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * App4Cam is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with App4Cam.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
  * Add types (that are not auto-magically added by Quasar CLI already)
  * for your custom variables to avoid TypeScript errors, like dynamic
  * process.env variables or definitions in dotenv files configured ONLY
